702afc16da3d361be2ef1565b04ab52be6402b6a
dylan.k <bee2side@gmail.com>
2020-02-25 08:38:09 +0900

    메모 삭제 구현

--- dylanMemo/Data/DataManager.swift
+++ dylanMemo/Data/DataManager.swift
@@ -40,6 +40,13 @@ class DataManager {
         saveContext()
     }
     
+    func deleteMemo(_ memo: Memo?) {
+        if let memo = memo {
+            mainContenxt.delete(memo)
+            saveContext()
+        }
+    }
+    
     // MARK: - Core Data stack
 
     lazy var persistentContainer: NSPersistentContainer = {
